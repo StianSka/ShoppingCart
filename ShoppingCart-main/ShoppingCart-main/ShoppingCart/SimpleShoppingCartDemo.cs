14ca4bc1257f1212f008ab3ffdb0bb9b2a5548ae
StianSka <116058912+StianSka@users.noreply.github.com>
2023-04-17 13:17:04 +0200

    shopping cart

---
+++ ShoppingCart-main/ShoppingCart-main/ShoppingCart/SimpleShoppingCartDemo.cs
@@ -0,0 +1,30 @@
+﻿namespace ShoppingCart
+{
+    internal class SimpleShoppingCartDemo
+    {
+        public static void Run()
+        {
+            Product[] stock = new[] {
+                new Product("A",40),
+                new Product("B",70),
+                new Product("C",30)
+            };
+            Cart cart = new Cart();
+
+            cart.ShowCartContent();
+            Console.WriteLine();
+
+            cart.AddToCart(stock[0], 1);
+            cart.ShowCartContent();
+            Console.WriteLine();
+
+            cart.AddToCart(stock[1], 3);
+            cart.ShowCartContent();
+            Console.WriteLine();
+
+            cart.AddToCart(stock[0], 4);
+            cart.ShowCartContent();
+           
+        }
+    }
+}
