e715d95de660669facebc572dd630d9d1de7b57c
StianSka <116058912+StianSka@users.noreply.github.com>
2023-04-17 13:22:01 +0200

    a

---
+++ ShoppingCart/Cart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    internal class Cart
+    {
+        public List<OrderLine> OrderLines { get; }
+
+        public Cart() 
+        {
+            OrderLines = new List<OrderLine>();
+        }
+
+        public void ShowCartContent()
+        {
+            if (OrderLines.Count == 0)
+            {
+                Console.WriteLine("Handlekurven er tom.");
+                return;
+            }
+            var totalPrice = 0;
+            foreach (var line in OrderLines)
+            {
+                totalPrice += line.Product.Price * line.Count;
+                Console.WriteLine($"  {line.Count} stk. {line.Product.Name} kr {line.Product.Price} = {line.Product.Price * line.Count}");
+            }
+            Console.WriteLine($"Totalpris: {totalPrice}");
+        }
+
+        public void AddToCart(Product product, int count)
+        {
+            foreach (var line in OrderLines)
+            {
+                if (product == line.Product) { 
+                    line.Count += count;
+                    Console.WriteLine($"Du kjøpte {count} stk. {product.Name}");
+                    return;
+                }
+            }
+            OrderLines.Add( new OrderLine(product,count)); 
+            Console.WriteLine($"Du kjøpte {count} stk. {product.Name}");
+        }
+    }
+}
---
+++ ShoppingCart/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    internal class Product
+    {
+        public string Name { get; }
+        public int Price { get; }
+
+        public Product(string name, int price) 
+        { 
+            Name= name;
+            Price= price;
+        }
+    }
+}
